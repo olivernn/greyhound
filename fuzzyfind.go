e85f328d8b0bfbecb4972225bd496b2db2284c59
Oliver Nightingale <oliver.nightingale1@gmail.com>
2013-05-03 10:59:34 +0100

    filter directories by a query

--- fuzzyfind.go
+++ fuzzyfind.go
@@ -3,6 +3,9 @@ package main
 import (
   "fmt"
   "flag"
+  "os"
+  "path/filepath"
+  "regexp"
 )
 
 var query = flag.String("query", "", "The query")
@@ -10,5 +13,24 @@ var query = flag.String("query", "", "The query")
 func main () {
   flag.Parse()
 
-  fmt.Printf("%s", *query)
+  visit := func(path string, info os.FileInfo, err error) error {
+    if !info.IsDir() {
+      relPath, _ := filepath.Rel("/Users/olivernightingale/code/lunr.js", path)
+      match, _ := regexp.MatchString(*query, relPath)
+
+      if match {
+        fmt.Println(relPath)
+      }
+    } else {
+      match, _ := regexp.MatchString(".git", path)
+
+      if match {
+        return filepath.SkipDir
+      }
+    }
+
+    return nil
+  }
+
+  filepath.Walk("/Users/olivernightingale/code/lunr.js", visit)
 }
