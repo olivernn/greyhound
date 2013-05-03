706e722520182d06c9e0ab3c359fbc1adec47b5d
Oliver Nightingale <oliver.nightingale1@gmail.com>
2013-05-03 11:22:01 +0100

    convert query to pattern for matching

--- fuzzyfind.go
+++ fuzzyfind.go
@@ -6,17 +6,27 @@ import (
   "os"
   "path/filepath"
   "regexp"
+  "strings"
 )
 
 var query = flag.String("query", "", "The query")
 
+func stringToPattern(query string) string {
+  tokens := strings.Split(query, "")
+  pattern := strings.Join(tokens, ".*")
+  pattern = ".*" + pattern + ".*"
+  return pattern
+}
+
 func main () {
   flag.Parse()
+
+  pattern := stringToPattern(*query)
 
   visit := func(path string, info os.FileInfo, err error) error {
     if !info.IsDir() {
       relPath, _ := filepath.Rel("/Users/olivernightingale/code/lunr.js", path)
-      match, _ := regexp.MatchString(*query, relPath)
+      match, _ := regexp.MatchString(pattern, relPath)
 
       if match {
         fmt.Println(relPath)
